5a17bfd8e9597a6336abac2fcfe2527d77aa91f7
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-09-30 10:54:15 +0000

    Revert "configuração DRF"
    This reverts commit f76435ea84e13d4b70d2d36edfaad7e6710dfaa8.

---
+++ criacao_bd_sistemavendas.sql
@@ -0,0 +1,148 @@
+CREATE TABLE usuario (
+    id_usuario SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    telefone VARCHAR(20),
+    endereco TEXT,
+    data_nascimento DATE,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE produto (
+    id_produto SERIAL PRIMARY KEY ,
+    nome VARCHAR(150) NOT NULL,
+    descricao TEXT,
+    categoria VARCHAR(50),
+    preco DECIMAL(10,2) NOT NULL CHECK (preco >= 0),
+    quantidade_estoque INT DEFAULT 0 CHECK (quantidade_estoque >= 0),
+    peso DECIMAL(8,3),
+    dimensoes VARCHAR(50),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TYPE status_pedido_enum AS ENUM (
+    'pendente',
+    'confirmado',
+    'processando',
+    'enviado',
+    'entregue',
+    'cancelado'
+);
+
+
+-- CREATE TABLE pedido (
+--     id_pedido SERIAL PRIMARY KEY ,
+--     id_usuario INT NOT NULL,
+--     data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     status_pedido ENUM('pendente', 'confirmado', 'processando', 'enviado', 'entregue', 'cancelado') DEFAULT 'pendente',
+--     valor_total DECIMAL(12,2) DEFAULT 0.00 CHECK (valor_total >= 0),
+--     endereco_entrega TEXT NOT NULL,
+--     observacoes TEXT,
+--     data_entrega_prevista DATE,
+--     data_entrega_real DATE,
+  
+--     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE RESTRICT
+-- );
+
+
+CREATE TABLE pedido (
+    id_pedido SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status_pedido status_pedido_enum DEFAULT 'pendente',
+    valor_total DECIMAL(12,2) DEFAULT 0.00 CHECK (valor_total >= 0),
+    endereco_entrega TEXT NOT NULL,
+    observacoes TEXT,
+    data_entrega_prevista DATE,
+    data_entrega_real DATE,
+  
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE RESTRICT
+);
+
+CREATE TABLE itens_pedido (
+    id_item SERIAL PRIMARY KEY,
+    id_pedido INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL CHECK (quantidade > 0),
+    preco_unitario DECIMAL(10,2) NOT NULL CHECK (preco_unitario >= 0),
+    subtotal DECIMAL(12,2) GENERATED ALWAYS AS (quantidade * preco_unitario) STORED,
+  
+    FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido) ON DELETE CASCADE,
+    FOREIGN KEY (id_produto) REFERENCES produto(id_produto) ON DELETE RESTRICT,
+   	CONSTRAINT unique_pedido_produto UNIQUE (id_pedido, id_produto)
+);
+
+INSERT INTO usuario (nome, email, telefone, endereco, data_nascimento) VALUES
+('João Silva', 'joao.silva@email.com', '(11) 99999-1111', 'Rua A, 123, São Paulo - SP', '1990-05-15'),
+('Maria Santos', 'maria.santos@email.com', '(11) 99999-2222', 'Av. B, 456, São Paulo - SP', '1985-08-20'),
+('Pedro Oliveira', 'pedro.oliveira@email.com', '(11) 99999-3333', 'Rua C, 789, São Paulo - SP', '1992-12-10');
+
+INSERT INTO produto (nome, descricao, categoria, preco, quantidade_estoque, peso) VALUES
+('Smartphone Galaxy', 'Smartphone Android com 128GB', 'Eletrônicos', 1200.00, 50, 0.200),
+('Notebook Dell', 'Notebook Intel i5, 8GB RAM, 256GB SSD', 'Informática', 2500.00, 20, 2.100),
+('Tênis Nike Air', 'Tênis esportivo para corrida', 'Calçados', 350.00, 100, 0.800),
+('Livro Python', 'Livro sobre programação em Python', 'Livros', 89.90, 30, 0.500),
+('Mouse Gamer', 'Mouse óptico para jogos', 'Informática', 120.00, 75, 0.150);
+
+INSERT INTO produto (nome, descricao, categoria, preco, quantidade_estoque, peso, ativo) VALUES
+('PS5', 'PS5 nova geração', 'Eletrônicos', 5000.00, 0, 1, False)
+
+-- Itens dos pedidos
+INSERT INTO itens_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES
+(4, 1, 1, 1200.00)  -- João: 1 Smartphone
+
+-- Pedidos
+INSERT INTO pedido (id_usuario, endereco_entrega, observacoes) VALUES
+(1, 'Rua A, 123, São Paulo - SP', 'Entregar no período da manhã'),
+(2, 'Av. B, 456, São Paulo - SP', 'Apartamento 302'),
+(1, 'Rua A, 123, São Paulo - SP', 'Presente de aniversário');
+
+-- Pedidos
+INSERT INTO pedido (id_usuario, endereco_entrega, observacoes, status_pedido) VALUES
+(2, 'Av. B, 456, São Paulo - SP', 'Apartamento 302', 'enviado')
+
+
+
+-- Itens dos pedidos
+INSERT INTO itens_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES
+(1, 1, 1, 1200.00),  -- João: 1 Smartphone
+(1, 5, 2, 120.00),   -- João: 2 Mouses
+(2, 2, 1, 2500.00),  -- Maria: 1 Notebook
+(2, 3, 1, 350.00),   -- Maria: 1 Tênis
+(3, 4, 3, 89.90);    -- João: 3 Livros
+
+
+
+CREATE TABLE cliente (
+    cliente_id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,  
+    telefone VARCHAR(15),
+    email VARCHAR(100)
+)
+
+
+CREATE TABLE endereco (
+    endereco_id SERIAL PRIMARY KEY,
+    cliente_id INT REFERENCES cliente(cliente_id),
+    rua VARCHAR(255) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    cep VARCHAR(10) NOT NULL
+);
+
+
+CREATE TABLE forma_pagamento (
+    forma_pagamento_id SERIAL PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL
+)
+
+
+CREATE TABLE vendedor (
+    vendedor_id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255),
+    telefone VARCHAR(15)
+);
